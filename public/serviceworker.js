50b4031876f3670c6da4a7872963f6702e14e2ad
MRJAHID <mrjahid350@gmail.com>
2022-08-20 12:36:08 +0600

    PWA

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,41 +1,44 @@
 const CACHE_NAME = "version-1";
-const urlsToCache = ["index.html", "offline.html"];
+const urlsToCache = [ 'index.html', 'offline.html' ];
 
 const self = this;
 
-// Install a SERVICE WORKER
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log("Open Cache");
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
+// Install SW
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Opened cache');
 
-// Listen for Request
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cache) => {
-      return fetch(event.request).catch(() => caches.match("offline.html"));
-    })
-  );
-});
-
-// Activate the service worker
-self.addEventListener("activate", (event) => {
-  const cacheWhitelist = [];
-  cacheWhitelist.push(CACHE_NAME);
-
-  event.waitUntil(
-    caches.keys().then((cacheNames) =>
-      Promise.all(
-        cacheNames.map((cacheName) => {
-          if (!cacheWhitelist.includes(cacheName)) {
-            return caches.delete(cacheName);
-          }
-        })
-      )
+                return cache.addAll(urlsToCache);
+            })
     )
-  );
+});
+
+// Listen for requests
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(() => {
+                return fetch(event.request)
+                    .catch(() => caches.match('offline.html'))
+            })
+    )
+});
+
+// Activate the SW
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [];
+    cacheWhitelist.push(CACHE_NAME);
+
+    event.waitUntil(
+        caches.keys().then((cacheNames) => Promise.all(
+            cacheNames.map((cacheName) => {
+                if(!cacheWhitelist.includes(cacheName)) {
+                    return caches.delete(cacheName);
+                }
+            })
+        ))
+
+    )
 });
